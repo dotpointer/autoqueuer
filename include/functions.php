51766a4d50c804fb4f351eadb50d28d8b73c44f5
Robert Klebe <dotpointer@gmail.com>
2017-09-19 20:58:25 +0200

    Moving settings to setup file, editing TODO file.

--- include/functions.php
+++ include/functions.php
@@ -79,59 +79,8 @@
 	
 */
 
-	# database setup
-	# define('DATABASE_HOST', 'localhost');
-	# define('DATABASE_USERNAME', 'root');
-	# define('DATABASE_PASSWORD', '');
-	define('PROJECT_FILENAME', 'autoqueuer');
-	define('PROJECT_TITLE', 'Autoqueuer');
-
-	define('DATABASE_NAME', PROJECT_FILENAME);
-
-	# file to store eMule web session id in, must be writable
-	define('FILE_SESSION', '/var/cache/'.PROJECT_FILENAME.'.emule.session');
-
-	# tree root where to mount samba shares - ends with a slash
-	define('MOUNT_ROOTPATH', '/mnt/'.PROJECT_FILENAME.'/');
-
-	# folder to store preview files, must be read and writeable by www-data user
-	define('PREVIEW_DIR', '/examplehost/download/'.PROJECT_FILENAME.'-preview/');
-
-	# eMule setup - eMule MUST have special XML-template loaded as web interface
-	# define('EMULEHOST', 'hostname');
-	# define('EMULEWEBURL', 'http://'.EMULEHOST.':4711/');
-
-   # path to a suitable mailing application, set to false to disable
-   # not used in favour of php mail function
-   # define('MAILER', '/var/scripts/sysmail ###EMAIL### ###SUBJECT### ###BODY###');
-
-	# bad words detected in filenames
-	$blacklist = array('preteen', 'child', 'underage', '.exe','.zip', '.com', '.rar', 'kids', 'stargate');
-
-	# filetypes to index in lowercase
-	$indexable_file_endings = array(
-		'avi',
-		'divx',
-		'gif',
-		'jpeg',
-		'jpg',
-		'mov',
-		'mp4',
-		'mpeg',
-		'mpg',
-		'png',
-		'ram',
-		'rm',
-		'rv',
-		'tif',
-		'tiff',
-		'wmv',
-		'xvid'
-	);
-
-	# setup the locale
-	setlocale(LC_ALL, 'sv_SE.utf8');
-	setlocale(LC_NUMERIC, 'en_US.utf8'); # overwrite decimal separator
+	# open this file and edit to setup
+	require_once('setup.php');
 
 	# --- end of setup ---------------------------------------------------
 
@@ -437,8 +386,6 @@
 
 			global $messages;
 			global $verbose;
-
-			$levels = is_array($levels) ? $levels : array(VERBOSE_ERROR);
 
 			$tmp = $mashed ? '' : array();
 			$first = true;
